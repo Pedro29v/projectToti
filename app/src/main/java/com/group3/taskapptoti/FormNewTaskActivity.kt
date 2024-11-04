f3af64b2bcb6ddf532a5dbf07b137dd293a34fcb
unknown <pedrocastillo24.pc@gmail.com>
2024-11-04 17:16:49 -0300

    create navigation home to form new task

---
+++ app/src/main/java/com/group3/taskapptoti/FormNewTaskActivity.kt
@@ -0,0 +1,15 @@
+package com.group3.taskapptoti
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class FormNewTaskActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_new_task)
+    }
+}
