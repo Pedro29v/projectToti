2180f4d8cdcd17dec58c66bdaf6a2942969b579a
unknown <pedrocastillo24.pc@gmail.com>
2024-11-08 10:17:53 -0300

    show the recyclerview in the fragment

--- app/src/main/java/com/group3/taskapptoti/TaskListFragment.kt
+++ app/src/main/java/com/group3/taskapptoti/TaskListFragment.kt
@@ -8,39 +8,47 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-
 class TaskListFragment : Fragment() {
 
     private lateinit var recyclerView: RecyclerView
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_task_list, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_task_list, container, false)
+        recyclerView = view.findViewById(R.id.list_item)
+
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
-         recyclerView = view.findViewById<RecyclerView>(R.id.list_item)
-
-
-        recyclerView.layoutManager = LinearLayoutManager(context)
+        val dataset = listTask()
+        val customAdapter = TaskAdapter(dataset)
 
 
-
-        /*recyclerView.adapter = YourAdapter()*/ // Reemplaza con tu adaptador específico
-
-
+        recyclerView.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        recyclerView.adapter = customAdapter
     }
 
+    private fun listTask(): List<Task> {
+        return listOf(
+            Task("Levar crianças na escola", "Às 07:00"),
+            Task("Fazer compras", "Às 11:00"),
+            Task("Dar aula", "Às 19:00"),
+            Task("Jogar Red Dead Redemption 2", "Às 22:00"),
+            Task("Dormir", "Às 23:00"),
+            Task("Trablhar", "fazer entregas do ifood"),
 
+
+
+        )
+    }
 }
+
+
+
