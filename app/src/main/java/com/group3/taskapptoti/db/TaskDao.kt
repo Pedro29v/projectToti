e6b0cc02aa572fdff414ff10e6226020a7afbee2
unknown <pedrocastillo24.pc@gmail.com>
2024-11-20 11:47:19 -0300

    create Dao insert

---
+++ app/src/main/java/com/group3/taskapptoti/db/TaskDao.kt
@@ -0,0 +1,12 @@
+package com.group3.taskapptoti.db
+
+import androidx.room.Dao
+import androidx.room.Insert
+import com.group3.taskapptoti.model.Task
+
+@Dao
+interface TaskDao {
+
+    @Insert
+    fun insertAll(vararg task: Task)
+}
